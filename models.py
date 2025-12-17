7ad50ccbcba84c4628a0caabab6b308452b0a3cb
Ильин Глеб <ilingleb1608@gmail.com>
2025-12-17 11:02:01 +0700

    Редактирование

--- models.py
+++ models.py
@@ -21,12 +21,16 @@ class User(db.Model):
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+                          nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+                            nullable=False)
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     deleted_by_sender = db.Column(db.Boolean, default=False)
     deleted_by_receiver = db.Column(db.Boolean, default=False)
 
-    sender = db.relationship('User', foreign_keys=[sender_id], backref='sent_messages')
-    receiver = db.relationship('User', foreign_keys=[receiver_id], backref='received_messages')
+    sender = db.relationship('User', foreign_keys=[sender_id],
+                             backref='sent_messages')
+    receiver = db.relationship('User', foreign_keys=[receiver_id],
+                               backref='received_messages')
